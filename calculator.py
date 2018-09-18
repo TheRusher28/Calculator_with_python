ebf8b9e2da3afd19f6c71bbeeaf07206fb260478
TheRusher28 <therusher28@gmail.com>
2018-09-18 20:40:52 +0200

    Adding the code of the project :)

---
+++ calculator.py
@@ -0,0 +1,15 @@
+def addition(num_uno, num_dos):
+	"""Returns the result of a addition"""
+	return num_uno + num_dos
+
+def subtraction(num_uno, num_dos):
+	"""Returns the result of a substraction"""
+	return num_uno - num_dos
+
+def  multiplication(num_uno, num_dos):
+	"""Returns the result of a multiplication"""
+	return num_uno * num_dos
+
+def division(num_uno, num_dos):
+	"""Returns the result of a division"""
+	return num_uno / num_dos
---
+++ main.py
@@ -0,0 +1,16 @@
+from calculator import addition
+from calculator import subtraction
+from calculator import multiplication
+from calculator import division
+
+num_uno = input('Please, type the first number of your operation :')
+num_dos = input('Please, type the second number of your operation :')
+operation = input('What operation do you need? ')
+if operation == 'addition':
+    print(addition(float(num_uno), float(num_dos)))
+elif operation == 'subtraction':
+    print(subtraction(float(num_uno), float(num_dos)))
+elif operation == 'multiplication':
+    print(multiplication(float(num_uno), float(num_dos)))
+elif operation == 'division':
+    print(division(float(num_uno), float(num_dos)))
